582d7bdfa911334fe55e3494e64b13df552aa608
Peter Pirkelbauer <pirkelbauer2@llnl.gov>
2025-01-05 21:26:29 -0800

    (NAS/BT) update benchmark to work with non standard dirs
    updated benchmark and made directories configurable

--- tests/nas-benchmarks/BT/perftest.sh
+++ tests/nas-benchmarks/BT/perftest.sh
@@ -8,6 +8,15 @@ shift 2
 # set optflags according to FAROS
 OPTFLAGS="$@"
 
+# find out if we compile with or w/o OpenMP
+ompflag=""
+
+for arg in "$@"; do
+  if [ "$arg" = "-fopenmp" ]; then
+    ompflag="$arg"
+  fi  
+done
+
 set -e
 
 if [[ $# -eq 0 ]]; then
@@ -19,8 +28,8 @@ fi
 echo "$comp $OPTFLAGS -c -o bt.o $src"
 $comp $OPTFLAGS -c -o bt.o $src
 
-echo "$comp -fopenmp -lm -o ../bin/bt.A bt.o ../common/c_print_results.o ../common/c_timers.o ../common/c_wtime.o"
-$comp -fopenmp -lm -o ../bin/bt.A bt.o ../common/c_print_results.o ../common/c_timers.o ../common/c_wtime.o
+echo "$comp $ompflag -lm -o ../bin/bt.A bt.o ../common/c_print_results.o ../common/c_timers.o ../common/c_wtime.o"
+$comp $ompflag -lm -o ../bin/bt.A bt.o ../common/c_print_results.o ../common/c_timers.o ../common/c_wtime.o
 
 export OMP_NUM_THREADS=24 
 ../bin/bt.A >out.txt
