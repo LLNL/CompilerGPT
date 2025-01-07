0731486fbf1103ffe69ee4bdd522da9bf92c2ca3
Peter Pirkelbauer <pirkelbauer2@llnl.gov>
2025-01-07 15:42:18 -0800

    (tests) update SW benchmark
    performance tests reads compiler and optimization flag from
    compilergpt.

--- tests/smith-waterman/eval.sh
+++ tests/smith-waterman/eval.sh
@@ -1,19 +1,32 @@
 #!/usr/bin/env bash
 
-set -e
+if [[ $# -lt 2 ]]; then
+  echo "Error: Missing provided." >&2
+  echo "Usage: $0 srcfile compiler [compilerflags]" >&2
+  exit 1
+fi
+rm -f perf.bin
 
-if [[ $# -eq 0 ]]; then
-  echo "Error: No files provided." >&2
-  echo "Usage: $0 srcfile" >&2
+src=$1
+comp=$2
+
+shift 2
+
+# test for OpenMP code
+if grep -q "pragma omp" "$src"; then
+  echo "Do not use OpenMP!" >&2
   exit 1
 fi
 
-src=$1
+echo "$comp $@ perf.cc $src -o perf.bin"
 
-echo "/usr/bin/clang++ -O3 -march=native -DNDEBUG=1 perf.cc $src -o perf.bin"
+$comp "$@" ./perf.cc "$src" -o perf.bin
 
-/usr/bin/clang++ -O3 -march=native -DNDEBUG=1 ./perf.cc "$src" -o perf.bin
+success="$?"
+if [ $success -gt 0 ]; then
+  exit $success
+fi
 
 ./perf.bin
 
-exit 0
+exit "$?"
